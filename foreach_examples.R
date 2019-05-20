c4142f366e7495b6f0fd674a85389c532b1f692b
Kim <kimd15@nci-02069813-ml.nci.nih.gov>
2019-05-19 21:04:17 -0400

    Updated foeach_examples.R.

--- foreach_examples.R
+++ foreach_examples.R
@@ -16,17 +16,26 @@ foreach(i=1:100) %do% {
 # Do it parallel using foreach
 ## first register parallel backend
 registerDoParallel(cores = 4) 
+
+# WINDOWS
+# clusters <-  makeCluster(4) (create 4 worker cluster)
+# registerDoParallel(cl = clusters)
+
+# Check if you have registered the parallel backend and
+# also how many workers you have assigned.
+foreach::getDoParRegistered()
+foreach::getDoParWorkers()
+
 foreach(i=1:100) %dopar% {
     sqrt(i)
 }
 
-# Let's move on to more complicated example
 # Generate 1000 by 1000 matrix
-registerDoParallel(cores = 4)
 foreach(i=1:1000) %dopar% {
     rnorm(1000,mean = i, sd = sqrt(i))
-} # but this is not in matrix, we have list of columns,
-  # so how do we combine these at once without looping through the list
+} 
+# but this is not in matrix, we have list of columns,
+# so how do we combine these at once without looping through the result
 
 # use .combine
 mat <- foreach(i=1:1000, .combine = cbind) %dopar% {
@@ -35,16 +44,14 @@ mat <- foreach(i=1:1000, .combine = cbind) %dopar% {
 dim(mat)
 class(mat)
 
-# Let's find median of each column vector
+# median of each column vector
 medians <- foreach(col=iter(mat, by = "column"), .combine = "c") %dopar% {
     median(col)
 }
-dim(medians)
-class(medians)
+medians
 
-
-# What if we want to calculate squre of sum of elements for every vector of which index is multiple of 10, 
-# but  median for everything else?
+# calculate squre of sum of elements for every vector of which the index is multiple of 10, 
+# but median for everything else?
 res1 <- foreach(col=iter(mat, by = "column"), i=icount(), .combine = "c") %dopar% {
     if ( (i %% 10) == 0){
         sum(col)^2
@@ -54,7 +61,7 @@ res1 <- foreach(col=iter(mat, by = "column"), i=icount(), .combine = "c") %dopar
     }
 }
 
-# We now actually want to skip columns of which its median is less than 800, and calculate variance
+# skip columns of which its median is less than 800, and calculate variance on those that meet the condition
 res2 <- foreach(col=iter(mat, by = "column"), m=iter(medians), .combine = rbind) %:% when(m >= 800) %dopar% {
     c(var(col),m)
 }
